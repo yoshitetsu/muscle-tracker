e4b6183401041bc59859766dfdfaeba7f01b24e9
yoshitetsu <yoshida.tetsuhiro@grimoire.co>
2026-04-09 12:45:11 +0900

    fix: use @libsql/client/web for Vercel serverless

--- app/.server/db/index.ts
+++ app/.server/db/index.ts
@@ -1,8 +1,12 @@
+import { createClient as createWebClient } from '@libsql/client/web'
+import { createClient as createLocalClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import { dbCredentials } from '~/config/.server/db'
+import { isProd } from '~/config/env'
 import * as schema from './schema'
 
-export const db = drizzle({
-  connection: dbCredentials,
-  schema,
-})
+let client = isProd
+  ? createWebClient(dbCredentials)
+  : createLocalClient(dbCredentials)
+
+export const db = drizzle(client, { schema })
